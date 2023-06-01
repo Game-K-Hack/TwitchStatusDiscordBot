c3a7f5fcf18eeb4df97e7b0eb257a280973309db
Game K <57108380+Game-K-Hack@users.noreply.github.com>
2023-06-01 14:57:17 +0200

    change live message to embed message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands, tasks
 
 default_intents = discord.Intents.default().all()
 default_intents.members = True
-client = commands.Bot(command_prefix="!ts ", help_command=None, intents=default_intents, activity=discord.Streaming(name="Status: !ts help", url="https://www.twitch.tv/game_k_"))
+client = commands.Bot(command_prefix="!ts beta ", help_command=None, intents=default_intents, activity=discord.Streaming(name="Status: !ts help", url="https://www.twitch.tv/game_k_"))
 
 try:
     with open("env.pkl", "rb") as file:
@@ -45,6 +45,44 @@ def is_stream(channel:str):
             "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"
         }).json()["data"]["user"]["stream"] != None
 
+def create_embed(channel):
+    res = requests.post(
+        "https://gql.twitch.tv/gql",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0",
+            "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"
+        },
+        json=[
+            {"operationName": "ChannelShell","variables": {"login": channel},"extensions": {"persistedQuery": {"version": 1,"sha256Hash": "580ab410bcd0c1ad194224957ae2241e5d252b2c5173d8e0cce9d32d5bb14efe"}}},
+            {"operationName": "ExtensionsOverlay","variables": {"channelLogin": channel},"extensions": {"persistedQuery": {"version": 1,"sha256Hash": "0ac7d363a6f57917933f99a1066075cc38fcdb87c10643fbb2aeacf21399f7b9"}}},
+            {"operationName": "StreamTagsTrackingChannel","variables": {"channel": channel},"extensions": {"persistedQuery": {"version": 1,"sha256Hash": "6aa3851aaaf88c320d514eb173563d430b28ed70fdaaf7eeef6ed4b812f48608"}}},
+            {"operationName": "NielsenContentMetadata","variables": {"isCollectionContent": False,"isLiveContent": True,"isVODContent": False,"collectionID": "","login": channel,"vodID": ""},"extensions": {"persistedQuery": {"version": 1,"sha256Hash": "2dbf505ee929438369e68e72319d1106bb3c142e295332fac157c90638968586"}}},
+        ]
+    )
+
+    ChannelShell, ExtensionsOverlay, StreamTagsTrackingChannel, NielsenContentMetadata = eval(res.content.decode("utf8").replace("true", "True").replace("false", "False").replace("null", "''"))
+
+    ch = ChannelShell["data"]["userOrError"]
+    # id = ch["id"]
+    login = ch["login"]
+    displayName = ch["displayName"]
+    primaryColorHex = ch["primaryColorHex"]
+    profileImageURL = ch["profileImageURL"]
+
+    eo = ExtensionsOverlay["data"]["user"]
+    game = eo["stream"]["game"]["name"]
+
+    ncm = NielsenContentMetadata["data"]["user"]
+    title = ncm["broadcastSettings"]["title"]
+
+    embed = discord.Embed(title=f"{displayName} ({login})" if login.lower() != displayName.lower() else displayName,
+                      url="https://www.twitch.tv/" + channel,
+                      description=f"**{title}**\n*{game}*",
+                      colour=int(primaryColorHex, 16))
+    embed.set_thumbnail(url=profileImageURL)
+
+    return embed
+
 @client.event
 async def on_ready():
     init_channels()
@@ -58,7 +96,7 @@ async def task_loop():
         status = is_stream(channel)
         if not channel_status[channel] and status:
             ctx = client.get_channel(channel_id)
-            await ctx.send(f"**{channel}** est actuellement en live\nhttps://www.twitch.tv/{channel}")
+            # await ctx.send(f"**{channel}** est actuellement en live\nhttps://www.twitch.tv/{channel}")
         channel_status[channel] = status
     save()
 
@@ -86,8 +124,16 @@ async def help(ctx):
 async def live(ctx):
     live_channel = [channel for channel in channel_status.keys() if channel_status[channel]]
     if len(live_channel) > 0:
-        live_channel = "**\n- **".join(live_channel)
-        await ctx.send(f"Voici la liste des personnes actuellement en live:\n- **{live_channel}**")
+        # live_channel = "**\n- **".join(live_channel)
+        # await ctx.send(f"Voici la liste des personnes actuellement en live:\n- **{live_channel}**")
+        embedList = []
+        for lc in live_channel:
+            embed = create_embed(lc)
+            embedList.append(embed)
+            if(len(embedList) == 10):
+                await ctx.send(embeds=embedList)
+                embedList = []
+        await ctx.send(embeds=embedList)
     else:
         await ctx.send("😔 Désolé, d'après ma liste il n'y a pas de live")
 
